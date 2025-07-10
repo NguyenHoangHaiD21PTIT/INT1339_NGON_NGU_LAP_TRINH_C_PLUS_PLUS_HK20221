89637584f51a8116525bcb4e4b72e02b9406d8a8
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-10 11:00:22 +0700

    Sửa cách làm dễ hiểu hơn

--- CPP0273 - DIEM CAN BANG.cpp
+++ CPP0273 - DIEM CAN BANG.cpp
@@ -1,32 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-	int t;
-	cin>>t;
-	while(t--){
-		int n;
-		cin>>n;
-		int a[n];
-		int tongtrai=0, tong=0;
-		for(int i=0;i<n;i++){
-			cin>>a[i];
-			tong+=a[i];
-		}
-		int tongphai=tong;
-		//Ban dau voi so a[0] thi tongtrai=0, tongphai=tong-a[i]
-		int check=0;
-		for(int i=0;i<n;i++){
-			tongphai-=a[i];
-			if(tongphai==tongtrai){
-				cout<<i+1;
-				check=1;
-				break;
-			}
-			tongtrai+=a[i];	
-		}
-		if(check==0){
-			cout<<-1;
-		}
-		cout<<endl;
-	}
+int main() {
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        int a[n + 5], prefix[n + 5];
+        prefix[0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            cin >> a[i];
+            prefix[i] = prefix[i - 1] + a[i];
+        }
+        int res = -1;
+        for (int i = 1; i <= n; ++i) {
+            int left = prefix[i - 1]; // tổng từ a[1]..a[i-1]
+            int right = prefix[n] - prefix[i]; // tổng từ a[i+1]..a[n]
+            if (left == right) {
+                res = i;
+                break;
+            }
+        }
+        cout << res << '\n';
+    }
 }
