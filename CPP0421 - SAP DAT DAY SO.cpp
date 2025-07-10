cf16393a7324fca89d3e9d2562cefd2dc6bb4a47
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-10 11:03:09 +0700

    Update CPP0421 - SAP DAT DAY SO.cpp

--- CPP0421 - SAP DAT DAY SO.cpp
+++ CPP0421 - SAP DAT DAY SO.cpp
@@ -6,27 +6,16 @@ int main(){
 	while (t--){
 		long n; 
 		cin>>n;
-		long long a[n];
-		long long b[n];
-		for (int i=0; i<n; i++){
-			b[i]=-1;
-		}
-		for (int i=0; i<n; i++){
-			cin>>a[i];
-		}
+		long long a[n], b[n];
+		for (int i=0; i<n; i++) b[i]=-1;
+		for (int i=0; i<n; i++) cin>>a[i];
 		sort(a,a+n);
 		for (int i=0; i<n; i++){
 			for (int j=0; j<n; j++){
-				if (a[j]==i){
-					b[i]=a[j];
-				}
+				if (a[j]==i) b[i]=a[j];
 			}
 		}
-		for (int i=0; i<n; i++){
-			cout<<b[i]<<" ";
-		}
+		for (int i=0; i<n; i++) cout<<b[i]<<" ";
 		cout<<endl;
 	}
-	
 }
-
