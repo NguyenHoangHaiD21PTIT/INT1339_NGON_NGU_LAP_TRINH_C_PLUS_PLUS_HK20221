ecfdba019ca83714bd69b235a53a386a76f818dc
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-15 10:04:02 +0700

    Update CPP0526 - TRE NHAT - GIA NHAT.cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 5. CẤU TRÚC CƠ BẢN/CPP0526 - TRE NHAT - GIA NHAT.cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 5. CẤU TRÚC CƠ BẢN/CPP0526 - TRE NHAT - GIA NHAT.cpp
@@ -1,31 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-struct nguoi{
-	string ten,ns;
-	int ngay, thang, nam;
+
+struct Nguoi {
+    string ten, ns;
+    int ngay, thang, nam;
 };
-bool cmp(nguoi a, nguoi b){
-	a.nam=(a.ns[6]-'0')*1000+(a.ns[7]-'0')*100+(a.ns[8]-'0')*10+(a.ns[9]-'0');
-	b.nam=(b.ns[6]-'0')*1000+(b.ns[7]-'0')*100+(b.ns[8]-'0')*10+(b.ns[9]-'0');
-	a.thang=(a.ns[3]-'0')*10+(a.ns[4]-'0');
-	b.thang=(b.ns[3]-'0')*10+(b.ns[4]-'0');
-	a.ngay=(a.ns[0]-'0')*10+(a.ns[1]-'0');
-	b.ngay=(b.ns[0]-'0')*10+(b.ns[1]-'0');
-	if(a.nam!=b.nam){
-		return (a.nam<b.nam);
-	}
-	if(a.thang!=b.thang){
-		return (a.thang<b.thang);
-	}
-	return a.ngay<b.ngay;
+
+int getInt(const string &s, int pos, int len) {
+    return stoi(s.substr(pos, len));
 }
-int main(){
-	int n;
-	cin>>n;
-	nguoi a[n];
-	for(int i=0;i<n;i++){
-		cin>>a[i].ten>>a[i].ns;
-	}
-	sort(a,a+n, cmp);
-	cout<<a[n-1].ten<<endl<<a[0].ten;
+
+bool cmp(const Nguoi &a, const Nguoi &b) {
+    if (a.nam != b.nam) return a.nam < b.nam;
+    if (a.thang != b.thang) return a.thang < b.thang;
+    return a.ngay < b.ngay;
+}
+
+int main() {
+    int n; cin >> n;
+    vector<Nguoi> a(n);
+    for (auto &x : a) {
+        cin >> x.ten >> x.ns;
+        x.ngay = getInt(x.ns, 0, 2);
+        x.thang = getInt(x.ns, 3, 2);
+        x.nam = getInt(x.ns, 6, 4);
+    }
+    sort(a.begin(), a.end(), cmp);
+    cout << a.back().ten << endl << a.front().ten;
 }
