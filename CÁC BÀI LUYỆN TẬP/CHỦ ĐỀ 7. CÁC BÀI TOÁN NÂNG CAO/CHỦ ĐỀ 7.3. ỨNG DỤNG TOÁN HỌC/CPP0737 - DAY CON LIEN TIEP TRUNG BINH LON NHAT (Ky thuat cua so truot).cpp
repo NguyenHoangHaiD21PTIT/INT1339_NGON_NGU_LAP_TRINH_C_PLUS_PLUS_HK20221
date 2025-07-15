61a2292ee6ad166a264597aaeca19a5db7bc4bdd
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-15 10:04:29 +0700

    Update CPP0737 - DAY CON LIEN TIEP TRUNG BINH LON NHAT (Ky thuat cua so truot).cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 7. CÁC BÀI TOÁN NÂNG CAO/CHỦ ĐỀ 7.3. ỨNG DỤNG TOÁN HỌC/CPP0737 - DAY CON LIEN TIEP TRUNG BINH LON NHAT (Ky thuat cua so truot).cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 7. CÁC BÀI TOÁN NÂNG CAO/CHỦ ĐỀ 7.3. ỨNG DỤNG TOÁN HỌC/CPP0737 - DAY CON LIEN TIEP TRUNG BINH LON NHAT (Ky thuat cua so truot).cpp
@@ -1,4 +1,3 @@
-// KY THUAT CUA SO TRUOT
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
@@ -8,27 +7,23 @@ int main(){
 		int n, k;
 		cin>>n>>k;
 		int a[n];
-		for(int i=0;i<n;i++){
-			cin>>a[i];
-		}
-		//Tinh cua so truot k phan tu dau tien
-		long long tong=0, tongmax=0, chiso=0;//chi so la chi so dau tien cua day con dai k tong max
+		for(int i=0;i<n;i++) cin>>a[i];
+		//Tính tổng cửa sổ trượt đầu tiên
+		//tong là tổng của k phần tử đang xét, tongmax là đáp án, chỉ số là chỉ số bắt đầu của cửa sổ trượt có tổng lớn nhất
+		long long tong=0, tongmax=0, chiso=0;
 		for(int i=0;i<k;i++){
 			tong=tong+a[i];
 			tongmax=tong;
 		}
-		//de so i dang xet ton tai k-1 so sau no thi i(0->n-k+1)
-		for(int i=1;i<n-k+1;i++){
-			//tu day con thu 2, tong cua so truot dang xet = tong cstruot truoc do - phan tu vua bi bo + phan tu vua them vao
-			tong=tong-a[i-1]+a[i+k-1];
-			if(tong>=tongmax){
-				tongmax=tong;
-				chiso=i;
+		// (st; en) dài k --> en - st + 1 = k --> en = st + k - 1, en < n --> st < n - k + 1
+		for(int i = 1; i < n - k + 1; i++){
+			tong = tong - a[i-1] + a[i+k-1];
+			if(tong >= tongmax){
+				tongmax = tong;
+				chiso = i;
 			}
 		}
-		for(int i=chiso;i<=chiso+k-1;i++){
-			cout<<a[i]<<" ";
-		}
+		for(int i = chiso; i <= chiso + k - 1;i++) cout<<a[i]<<" ";
 		cout<<endl;
 	}
 }
