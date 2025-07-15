c5ad41d245f32e87080b2905ddd47543e2cb04b0
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-15 10:02:56 +0700

    Update CPP0230 - MA TRAN NHI PHAN.cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 2. MẢNG MỘT CHIỀU/CPP0230 - MA TRAN NHI PHAN.cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 2. MẢNG MỘT CHIỀU/CPP0230 - MA TRAN NHI PHAN.cpp
@@ -5,21 +5,15 @@ int main () {
 	cin >> n;
 	int a[n][3];
 	for(int i=0; i<n; i++) {
-		for(int j=0; j<3; j++) {
-			cin >> a[i][j];
-		}
+		for(int j=0; j<3; j++) cin >> a[i][j];
 	}
-	int dem_hang=0;
+	int ans = 0;
 	for(int i=0; i<n; i++) {
-		int dem_so1=0;
-		for(int j=0; j<3; j++) {//dem so so 1 o moi hang
-			if (a[i][j] ==1) {
-				dem_so1++;
-			}
+		int cnt1 = 0;
+		for(int j=0; j<3; j++) {
+			if (a[i][j] ==1) cnt1++;
 		}
-		if (dem_so1 >=2){
-			dem_hang++;
-		}
+		if (cnt1 >=2) ans++;
 	}
-	cout << dem_hang;
+	cout << ans;
 }
