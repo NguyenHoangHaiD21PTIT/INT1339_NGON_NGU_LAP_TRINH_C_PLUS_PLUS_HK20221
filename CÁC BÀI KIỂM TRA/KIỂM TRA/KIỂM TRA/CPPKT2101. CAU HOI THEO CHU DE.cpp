35fcca18167a355ff20bc3e4960a2c25133ea911
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-15 10:05:36 +0700

    Update CPPKT2101. CAU HOI THEO CHU DE.cpp

--- CÁC BÀI KIỂM TRA/KIỂM TRA/KIỂM TRA/CPPKT2101. CAU HOI THEO CHU DE.cpp
+++ CÁC BÀI KIỂM TRA/KIỂM TRA/KIỂM TRA/CPPKT2101. CAU HOI THEO CHU DE.cpp
@@ -1,21 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
     int n;
     cin >> n;
     cin.ignore(); 
     vector<string> topics;
     string topic;
-
     for (int i = 0; i < n; ++i) {
         getline(cin, topic);
         topics.push_back(topic);
-        if (topic=="") {
-            cout << topics[0] << ": " << topics.size() - 2 << endl;
+        if (topic=="") { //Kết thúc một chủ đề
+            cout << topics[0] << ": " << topics.size() - 2 << endl;//-2 là bỏ đi tên chủ đề và dòng trống
             topics.clear();
         }
     }
-
+    //Chủ đề cuối cùng không có dòng trống kết thúc
     if (!topics.empty()) cout << topics[0] << ": " << topics.size() - 1 << endl;
 } 
+
