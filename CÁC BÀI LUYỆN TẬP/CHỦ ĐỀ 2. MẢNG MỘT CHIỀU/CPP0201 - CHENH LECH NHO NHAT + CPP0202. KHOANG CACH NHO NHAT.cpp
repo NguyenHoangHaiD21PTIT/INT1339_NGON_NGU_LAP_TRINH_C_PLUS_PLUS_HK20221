e05e8ee8b1ddde709d932952e1cf525fc09e5f77
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-15 10:02:32 +0700

    Update CPP0201 - CHENH LECH NHO NHAT + CPP0202. KHOANG CACH NHO NHAT.cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 2. MẢNG MỘT CHIỀU/CPP0201 - CHENH LECH NHO NHAT + CPP0202. KHOANG CACH NHO NHAT.cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 2. MẢNG MỘT CHIỀU/CPP0201 - CHENH LECH NHO NHAT + CPP0202. KHOANG CACH NHO NHAT.cpp
@@ -1,22 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main(){
-int t;
-cin >> t;
-while(t--){
-	int n;
-	cin >> n;
-	int a[n];
-	for(int i=0;i<n;i++){
-		cin >> a[i];
+	int t;
+	cin >> t;
+	while (t--){
+		int n;
+		cin >> n;
+		int a[n];
+		for (int i = 0; i < n; i++) cin >> a[i];
+		sort(a, a + n);
+		int gtnn = 1e9;
+		for (int i = 1; i < n; i++) gtnn = min(gtnn, a[i] - a[i - 1]);
+		cout << gtnn << endl;
 	}
-	sort(a, a+n);
-	int gtnn=1e9;
-	for(int i=1;i<n;i++){
-		gtnn=std::min(gtnn, a[i]-a[i-1]);
-	}
-    cout << gtnn << endl;
 }
-}
-                        
+                  
  
