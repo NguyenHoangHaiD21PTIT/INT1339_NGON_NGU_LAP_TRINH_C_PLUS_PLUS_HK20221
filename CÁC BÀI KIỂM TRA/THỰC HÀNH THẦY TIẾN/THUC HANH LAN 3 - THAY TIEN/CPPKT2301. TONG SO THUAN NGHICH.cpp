8173dae2f0470cf2f4773721bbb8eb8e8ece613a
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-16 13:36:04 +0700

    Update CPPKT2301. TONG SO THUAN NGHICH.cpp

--- CÁC BÀI KIỂM TRA/THỰC HÀNH THẦY TIẾN/THUC HANH LAN 3 - THAY TIEN/CPPKT2301. TONG SO THUAN NGHICH.cpp
+++ CÁC BÀI KIỂM TRA/THỰC HÀNH THẦY TIẾN/THUC HANH LAN 3 - THAY TIEN/CPPKT2301. TONG SO THUAN NGHICH.cpp
@@ -23,7 +23,7 @@ Ví dụ 1: số có 6 chữ số. Ban đầu sẽ có 900 số [sum(10^2, 999) 
 ...
 999 000 
 Ví dụ: 123 000 --> 123 000 + 321 = 123 321
-Mỗi một abc trước đó sẽ có một cba tương ứng cũng có <=3 chữ số. VD: 100 -> 001, 101 --> 101, 102 -> 201, 103 -> 301, ..., 999 -> 999 --> Tính tổng từ 1 đến 999
+Mỗi một abc trước đó sẽ có một cba tương ứng cũng có <=3 chữ số. VD: 100 -> 001, 101 --> 101, 102 -> 201, 103 -> 301, ..., 999 -> 999 --> Tính tổng từ 001 đến 999
 abc không hợp lệ khi 
 + a = 0 -> 0bc --> Đảo không thể có dạng bc0, được tạo ra từ các số bc rồi nhân 10, với b c tuỳ ý --> bc 00 01 ... 99 --> Tính tổng từ 00 đến 99 rồi nhân 10 (Loại bỏ cac phần đảo 000, 010, ..., 090, 100, 110, ..., 990)
 Ví dụ 2: Số có 8 chữ số. abcddcba. Gen ban đầu 1000 0000, 1001 0000, ..., 9999 0000 (k = 4) (Gen phần đầu là tất cả các số có 4 chữ số) -> Sum(10^3, 9999) * 10000
@@ -36,8 +36,9 @@ left = 10^{k-1} → số nhỏ nhất có k chữ số
 right = 10^k - 1 → số lớn nhất có k chữ số
 ⇒ right * 10 + 9 = 10^{k+1} - 1 → số lớn nhất có k+1 chữ số
 --> Gen: 1000 000, 1001 000, 9999 000
-Mỗi abc trước đó sẽ có đảo tương ứng cba <=3 chữ số. VD: 100 -> 001, 101 --> 101, 102 -> 201, 103 -> 301, ..., 999 -> 999 --> Tính tổng từ 1 đến 999
-Nhưng chú ý là bây giờ abc ghép thêm d nữa, mà d có 10 cách nên mỗi cba đảo bị tính 10 lần. Chẳng hạn: 123 0 321, 123 1 321, ..., 123 9 321.
+Mỗi abc trước đó sẽ có đảo tương ứng cba <=3 chữ số. VD: 100 -> 001, 101 --> 101, 102 -> 201, 103 -> 301, ..., 999 -> 999 --> Tính tổng từ 001 đến 999
 abc không hợp lệ khi
 + a = 0 → 0bc → đảo không thể có dạng bc0 --> được tạo ra từ các số bc rồi nhân 10, với b c tuỳ ý → bc 00 01 ... 99 → tính tổng từ 00 đến 99 rồi nhân 10 (loại bỏ các phần đảo 000, 010, ..., 090, 100, 110, ..., 990)
+(Trừ như trường hợp vừa nãy)
+Nhưng chú ý là bây giờ abc ghép thêm d nữa, mà d có 10 cách nên mỗi cba đảo bị tính 10 lần. Chẳng hạn: 123 0 321, 123 1 321, ..., 123 9 321.
 */
