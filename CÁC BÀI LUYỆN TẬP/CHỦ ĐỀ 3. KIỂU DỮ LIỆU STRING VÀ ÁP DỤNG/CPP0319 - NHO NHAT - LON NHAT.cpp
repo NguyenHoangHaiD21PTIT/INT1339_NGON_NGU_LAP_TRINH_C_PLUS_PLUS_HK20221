694b8910415585275f042bda45f1929edc0cf2ff
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-16 08:44:04 +0700

    Update CPP0319 - NHO NHAT - LON NHAT.cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 3. KIỂU DỮ LIỆU STRING VÀ ÁP DỤNG/CPP0319 - NHO NHAT - LON NHAT.cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 3. KIỂU DỮ LIỆU STRING VÀ ÁP DỤNG/CPP0319 - NHO NHAT - LON NHAT.cpp
@@ -1,40 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-void solve(int s, int m){//tong chu so=s, so co m chu so
+void solve(int s, int m){//Tong chu so = s, so co m chu so
 	int tmp = s;
-	if(s > 9 *m || (s == 0 && m > 2)){
-		cout << "-1 -1";
-	}
-	else{
+	if(s > 9 *m || (s == 0 && m > 2)) cout << "-1 -1";
+	else {
 		vector<int> a(m), b(m);
 		for(int i = 0; i < m; i++){
-			if(s != 0){
-				if(s >= 9){
-					a[i] = 9; s -= 9;
-				}
-				else{
-					a[i] = s; s = 0;
-				}
-			}
-			else a[i] = s;
+			if(s >= 9) {
+				a[i] = 9; s -= 9;
+			} else {
+				a[i] = s; s = 0;
+			} 
 		}
-		--tmp;
+		tmp--;
 		for(int i = m - 1; i > 0; i--){
 			if(tmp >= 9){
 				b[i] = 9; tmp -= 9;
-			}
-			else if(tmp != 0){
+			} else {
 				b[i] = tmp; tmp = 0;
 			}
-			else b[i] = tmp;
 		}
 		b[0] = tmp + 1;
-		for(int x : b){
-			cout << x;
-		}
+		for(int x : b) cout << x;
 		cout << " ";
-		for(int x : a) 
-			cout << x;
+		for(int x : a) cout << x;
 	}
 }
  
