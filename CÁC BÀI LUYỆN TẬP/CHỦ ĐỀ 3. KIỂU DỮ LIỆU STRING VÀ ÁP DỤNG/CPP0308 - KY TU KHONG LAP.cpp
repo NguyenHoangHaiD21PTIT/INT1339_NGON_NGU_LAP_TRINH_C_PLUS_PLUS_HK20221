f3048846ca5d0139d9e7a4eb526cd4f3e24065fb
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-16 08:44:38 +0700

    Update CPP0308 - KY TU KHONG LAP.cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 3. KIỂU DỮ LIỆU STRING VÀ ÁP DỤNG/CPP0308 - KY TU KHONG LAP.cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 3. KIỂU DỮ LIỆU STRING VÀ ÁP DỤNG/CPP0308 - KY TU KHONG LAP.cpp
@@ -9,14 +9,10 @@ int main(){
 		memset(dem, 0, sizeof(dem));
 		string c;
 		cin >> c;
+		for(int i=0;i<c.size();i++) dem[c[i]]++;
 		for(int i=0;i<c.size();i++){
-			dem[c[i]-'0']++;
-		}
-		for(int i=0;i<c.size();i++){
-			if(dem[c[i]-'0']==1){
-				cout << c[i];
-			}
-			dem[c[i]-'0']=0;
+			if(dem[c[i]]==1) cout << c[i];
+			dem[c[i]]=0;
 		}
 		cout << endl;
 	}
