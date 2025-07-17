46513bc59343edf7c60b54d670210cc112ee29b9
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-17 16:56:16 +0700

    Update CPP0431 - DEM CAP PHAN TU CO HIEU NHO HON K.cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 4. KỸ THUẬT SẮP XẾP VÀ TÌM KIẾM/CPP0431 - DEM CAP PHAN TU CO HIEU NHO HON K.cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 4. KỸ THUẬT SẮP XẾP VÀ TÌM KIẾM/CPP0431 - DEM CAP PHAN TU CO HIEU NHO HON K.cpp
@@ -5,18 +5,17 @@ int main(){
 	cin>>t;
 	while(t--){
 		int n, k;
-		cin>>n>>k;
+		cin >> n >> k;
 		int a[n];
-		for(int i=0;i<n;i++){
-			cin>>a[i];
-		}
-		sort(a,a+n);
-		//x-a[i]<k => x<=a[i]+k-1 ==> Tim vi tri dau tien ma > a[i]+k-1--> Upper bound
+		for(int i =0;i < n; i++) cin >> a[i];
+		sort(a, a + n);
 		long long dem=0;
-		for(int i=0;i<n-1;i++){
-			auto r=upper_bound(a+i+1,a+n,a[i]+k-1);
-			dem+=1ll*(((r-a)-1)-(i+1)+1);//r-a: giai tham chieu
+		for(int i = 0; i < n-1; i++){
+			auto r = upper_bound (a + i + 1, a + n, a[i] + k - 1);
+			dem += 1ll *(((r- a)- 1) - (i + 1) + 1);
 		}
-		cout<<dem<<endl;
+		cout << dem << endl;
 	}
 }
+//r-a: giai tham chieu
+//x-a[i]<k => x<=a[i]+k-1 ==> Tim vi tri dau tien ma > a[i]+k-1--> Upper bound
