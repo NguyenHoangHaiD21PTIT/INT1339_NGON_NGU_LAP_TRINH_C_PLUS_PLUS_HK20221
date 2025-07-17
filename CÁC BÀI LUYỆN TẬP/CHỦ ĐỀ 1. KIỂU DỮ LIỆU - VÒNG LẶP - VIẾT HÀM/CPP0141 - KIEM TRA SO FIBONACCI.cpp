b11b31e19e62e4b03760d229cccd3f23406eac18
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-17 16:55:00 +0700

    Update CPP0141 - KIEM TRA SO FIBONACCI.cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 1. KIỂU DỮ LIỆU - VÒNG LẶP - VIẾT HÀM/CPP0141 - KIEM TRA SO FIBONACCI.cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 1. KIỂU DỮ LIỆU - VÒNG LẶP - VIẾT HÀM/CPP0141 - KIEM TRA SO FIBONACCI.cpp
@@ -5,8 +5,7 @@ void gen(){
 	f[0] = 0; f[1] = 1;
 	for(int i = 2;i<=91;i++) f[i] = f[i - 1] + f[i - 2];
 }
-
-bool find(long long f[], long long x) {
+bool find(long long x) {
     for(int i = 0; i < 92; i++) {
         if(f[i] == x) return true;
     }
@@ -18,7 +17,7 @@ int main(){
 	while(t--){
 		long long n; cin>>n;
 		gen();
-		if(find(f, n)) cout<<"YES\n";
+		if(find(n)) cout<<"YES\n";
 		else cout<<"NO\n";
 	}
 }
