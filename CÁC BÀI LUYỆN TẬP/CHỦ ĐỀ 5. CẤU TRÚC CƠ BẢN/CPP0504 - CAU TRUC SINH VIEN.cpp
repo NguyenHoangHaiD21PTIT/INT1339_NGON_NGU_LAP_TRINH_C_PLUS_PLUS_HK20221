0516724302ce6c9e0f2821cfe7d5dbcfb6ee08e1
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-17 07:49:25 +0700

    Update CPP0504 - CAU TRUC SINH VIEN.cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 5. CẤU TRÚC CƠ BẢN/CPP0504 - CAU TRUC SINH VIEN.cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 5. CẤU TRÚC CƠ BẢN/CPP0504 - CAU TRUC SINH VIEN.cpp
@@ -2,29 +2,17 @@
 using namespace std;
 struct SinhVien{
 	string ma="B20DCCN001";
-	string ten;
-	string lop;
-	string ns;
+	string ten, lop, ns;
 	float gpa;
 };
 void nhap(SinhVien &a){
 	getline(cin, a.ten);
-	cin>>a.lop;
-	cin>>a.ns;
-	cin>>a.gpa;
+	cin>>a.lop >>a.ns >>a.gpa;
 }
 void in(SinhVien a){
-    if(a.ns[1]=='/'){
-		a.ns="0"+a.ns;
-	} 
-    if(a.ns[4]=='/'){
-		a.ns.insert(3,"0");
-	}
-	cout <<a.ma<<" "<<a.ten<<" "<<a.lop<<" ";
-	//chuan: 12/12/2012, k chuan 1: 2/x/2012(Ki tu chi so 1 la dau /, chuan phai la 1 so)
-	//k chuan 2: 20/3/2012(ki tu thu 4 la dau /...)
-	cout<<a.ns <<" ";
-	cout <<fixed<<setprecision(2)<<a.gpa;
+    if(a.ns[1]=='/') a.ns="0"+a.ns;
+    if(a.ns[4]=='/') a.ns.insert(3,"0");
+    cout <<a.ma<<" "<<a.ten<<" "<<a.lop<<" "<<a.ns <<" "<<fixed<<setprecision(2)<<a.gpa;
 }
 int main(){
     struct SinhVien a;
