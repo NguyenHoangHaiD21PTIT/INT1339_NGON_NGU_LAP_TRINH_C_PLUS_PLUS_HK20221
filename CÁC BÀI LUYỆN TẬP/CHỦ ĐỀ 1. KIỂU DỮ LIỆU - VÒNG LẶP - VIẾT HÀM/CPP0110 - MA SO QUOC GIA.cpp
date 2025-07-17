eb521a18aefae05caa07e10df4b2374d217ca0cd
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-17 08:14:17 +0700

    Update CPP0110 - MA SO QUOC GIA.cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 1. KIỂU DỮ LIỆU - VÒNG LẶP - VIẾT HÀM/CPP0110 - MA SO QUOC GIA.cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 1. KIỂU DỮ LIỆU - VÒNG LẶP - VIẾT HÀM/CPP0110 - MA SO QUOC GIA.cpp
@@ -8,14 +8,10 @@ int main(){
 		char c[50];
 		cin >> c;
 		for(int i=0;i<strlen(c)-2;i++){
-			if(c[i]=='0'&&c[i+1]=='8'&&c[i+2]=='4'){
-				c[i]=c[i+1]=c[i+2]='x';
-			}
+			if(c[i]=='0'&&c[i+1]=='8'&&c[i+2]=='4') c[i]=c[i+1]=c[i+2]='x';
 		}
 		for(int i=0;i<strlen(c);i++){
-			if(c[i]!='x'){
-				cout << c[i];
-			}
+			if(c[i]!='x') cout << c[i];
 		}
 		cout << endl;
 	}
