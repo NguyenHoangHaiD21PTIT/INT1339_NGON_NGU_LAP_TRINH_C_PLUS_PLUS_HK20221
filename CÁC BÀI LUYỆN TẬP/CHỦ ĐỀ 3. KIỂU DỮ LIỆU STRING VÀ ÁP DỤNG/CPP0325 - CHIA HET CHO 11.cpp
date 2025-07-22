309c7a3af0d12d949eade150fbdac7c8abcca263
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-22 07:51:13 +0700

    Update CPP0325 - CHIA HET CHO 11.cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 3. KIỂU DỮ LIỆU STRING VÀ ÁP DỤNG/CPP0325 - CHIA HET CHO 11.cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 3. KIỂU DỮ LIỆU STRING VÀ ÁP DỤNG/CPP0325 - CHIA HET CHO 11.cpp
@@ -7,19 +7,12 @@ int main(){
 	while(t--){
 		char c[1500];
 		cin>>c;
-		int tongchan=0, tongle=0;//tong chu so hang chan, hang le
+		int tongchan = 0, tongle = 0;
 		for(int i=0;i<strlen(c);i++){
-			if(i%2==0){
-				tongchan+=c[i]-'0';
-			} else {
-				tongle+=c[i]-'0';
-			}
+			if(i % 2 == 0) tongchan += c[i] - '0';
+			else tongle += c[i] - '0';
 		}
-		if((tongchan-tongle)%11==0){
-			cout <<"1" <<endl;
-		} else {
-			cout <<"0"<<endl;
-		}
+		if((tongchan - tongle) % 11 == 0) cout <<"1" << endl;
+		else cout << "0" << endl;
 	}
 }
-			
