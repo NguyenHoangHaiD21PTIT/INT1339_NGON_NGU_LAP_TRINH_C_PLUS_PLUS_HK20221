dcbcde96ba4348bb73f3a3a7615dcaa86215f403
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-22 08:16:20 +0700

    Update CPP0527 - SAP XEP THOI GIAN.cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 5. CẤU TRÚC CƠ BẢN/CPP0527 - SAP XEP THOI GIAN.cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 5. CẤU TRÚC CƠ BẢN/CPP0527 - SAP XEP THOI GIAN.cpp
@@ -1,26 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-struct tgian{
-	int gio, phut, giay;
-	int tong;
+struct Time {
+    int h, m, s;
 };
-bool cmp(tgian a, tgian b){
-	a.tong=a.gio*3600+a.phut*60+a.giay;
-	b.tong=b.gio*3600+b.phut*60+b.giay;
-	if(a.tong!=b.tong){
-		return a.tong<b.tong;
-	}
+bool cmp(const Time &a, const Time &b) {
+    if (a.h != b.h) return a.h < b.h;
+    if (a.m != b.m) return a.m < b.m;
+    return a.s < b.s;
 }
-int main(){
-	int n;
-	cin>>n;
-	tgian a[n];
-	for(int i=0;i<n;i++){
-		cin>>a[i].gio>>a[i].phut>>a[i].giay;
-	}
-	sort(a,a+n,cmp);
-	for(int i=0;i<n;i++){
-		cout<<a[i].gio<<" "<<a[i].phut<<" "<<a[i].giay<<endl;
-	}
+int main() {
+    int N; cin >> N;
+    vector<Time> times(N);
+    for (int i = 0; i < N; i++) cin >> times[i].h >> times[i].m >> times[i].s;
+    sort(times.begin(), times.end(), cmp);
+    for (auto &t : times) cout << t.h << " " << t.m << " " << t.s << endl;
 }
-	
+
