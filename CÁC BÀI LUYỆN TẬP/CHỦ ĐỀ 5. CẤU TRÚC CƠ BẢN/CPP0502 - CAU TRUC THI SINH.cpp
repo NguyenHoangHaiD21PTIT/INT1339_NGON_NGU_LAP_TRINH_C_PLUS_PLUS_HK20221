8fa5ad2373f8a81f5790ba83e54cad8e0eb0cba9
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-22 09:17:26 +0700

    Update CPP0502 - CAU TRUC THI SINH.cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 5. CẤU TRÚC CƠ BẢN/CPP0502 - CAU TRUC THI SINH.cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 5. CẤU TRÚC CƠ BẢN/CPP0502 - CAU TRUC THI SINH.cpp
@@ -1,17 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 struct ThiSinh{
-	string hoten;
-	string ngaysinh;
-	float x1, x2, x3;//diem thi 3 mon
+	string hoten, ngaysinh;
+	float x1, x2, x3;
 };
 void nhap(ThiSinh &a){
 	getline(cin, a.hoten);
-	cin>>a.ngaysinh;
-	cin>>a.x1>>a.x2>>a.x3;
+	cin >> a.ngaysinh >> a.x1 >> a.x2 >> a.x3;
 }
 void in(ThiSinh a){
-	cout<<a.hoten<<" " <<a.ngaysinh<<" "<<fixed<<setprecision(1)<<a.x1+a.x2+a.x3;
+	cout << a.hoten << " " << a.ngaysinh << " " << fixed << setprecision(1) << a.x1 + a.x2 + a.x3;
 }
 int main(){
     struct ThiSinh A;
