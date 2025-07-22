57da41cda9ab0007ef23b8dcc86d717a35da0f9d
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-22 08:10:46 +0700

    Update CPP0805 - HOP VA GIAO CUA HAI FILE VAN BAN.cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 8. VÀO RA TRÊN TỆP/CPP0805 - HOP VA GIAO CUA HAI FILE VAN BAN.cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 8. VÀO RA TRÊN TỆP/CPP0805 - HOP VA GIAO CUA HAI FILE VAN BAN.cpp
@@ -1,39 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-string chuanhoa(string &s){//viet thuong toan bo cac chu cai trong xau s
-	for(int i=0;i<s.size();i++){
-		s[i]=tolower(s[i]);
-	}
+string chuanhoa(string &s){
+	for(int i = 0;i < s.size();i++) s[i] = tolower(s[i]);
 	return s;
 }
 int main(){
 	fstream in1, in2;
 	in1.open("DATA1.in");
 	in2.open("DATA2.in");
-	set<string>sstr1;//loai cac tu trung nhau va viet thuong cac tu file DATA1
-	set<string>sstr2;//loai cac tu trung nhau va viet thuong cac tu file DATA2
-	set<string>sstr3;//hop hai tap tu
-	string s1, s2;//nhap xau file 1 va 2
-	while(in1>>s1){//nhap file 1
+	set <string> sstr1, sstr2, sstr3;
+	string s1, s2;
+	while(in1 >> s1){
 		string s;
-		s=chuanhoa(s1);
+		s = chuanhoa(s1);
 		sstr1.insert(s);
 		sstr3.insert(s);
 	}
-	while(in2>>s2){//nhap file 2
+	while(in2 >> s2){
 		string s;
-		s=chuanhoa(s2);
+		s = chuanhoa(s2);
 		sstr2.insert(s);
 		sstr3.insert(s);
 	}
-	for(auto x: sstr3){
-		cout<<x<<" ";
-	}
-	cout<<endl;
-	for(auto x: sstr1){//duyet tat ca cac tu trong set 1 xem co trong set 2 khong
-		if(sstr2.find(x)!=sstr2.end()){
-			cout<<x<<" ";
-		}
+	for(auto x: sstr3) cout<<x<<" ";
+	cout << endl;
+	for(auto x: sstr1){
+		if(sstr2.find(x) != sstr2.end()) cout << x << " ";
 	}
 }
 	
