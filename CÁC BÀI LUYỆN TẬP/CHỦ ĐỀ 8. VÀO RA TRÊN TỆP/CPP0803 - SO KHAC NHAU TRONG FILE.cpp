c1de8b4972e74bb557f67d002d1101b1c9d4e513
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-23 07:52:11 +0700

    Update CPP0803 - SO KHAC NHAU TRONG FILE.cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 8. VÀO RA TRÊN TỆP/CPP0803 - SO KHAC NHAU TRONG FILE.cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 8. VÀO RA TRÊN TỆP/CPP0803 - SO KHAC NHAU TRONG FILE.cpp
@@ -2,13 +2,9 @@
 using namespace std;
 int main(){
 	int n;
-	map<int,int>mp;
+	map <int, int> mp;
 	fstream infile;
 	infile.open("DATA.in");
-	while(infile>>n){
-		mp[n]++;
-	}
-	for(auto x:mp){
-		cout<<x.first<<" "<<x.second<<endl;
-	}
+	while (infile >> n) mp[n]++;
+	for(auto x:mp) cout << x.first << " " << x.second << endl;
 }
