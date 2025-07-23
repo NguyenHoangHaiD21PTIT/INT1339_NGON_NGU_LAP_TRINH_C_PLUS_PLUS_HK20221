72b8640492a263c807d00094c183af39c27fa231
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-23 22:59:16 +0700

    Update CPP0322 - TONG HAI SO NGUYEN LON.cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 3. KIỂU DỮ LIỆU STRING VÀ ÁP DỤNG/CPP0322 - TONG HAI SO NGUYEN LON.cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 3. KIỂU DỮ LIỆU STRING VÀ ÁP DỤNG/CPP0322 - TONG HAI SO NGUYEN LON.cpp
@@ -1,45 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void cong(string a, string b){
-	int n1=a.size();
-    int n2=b.size();
-    int x[n1], y[n1], z[n1+1];//x, y: mang luu cac so trong a, b; z: mang luu cac so trong tong
-    for(int i=0;i<n1;i++){
-    	x[i]=a[i]-'0';
+
+string addBigNumbers(string X, string Y) {
+    if (X.size() < Y.size()) swap(X, Y);
+    reverse(X.begin(), X.end());
+    reverse(Y.begin(), Y.end());
+    while (Y.size() < X.size()) Y += '0';
+    string res = "";
+    int carry = 0;
+    for (int i = 0; i < X.size(); i++) {
+        int digitX = X[i] - '0', digitY = Y[i] - '0';
+        int sum = digitX + digitY + carry;
+        res.push_back(sum % 10 + '0');
+        carry = sum / 10;
     }
-    for(int i=0;i<n2;i++){
-    	y[i]=b[i]-'0';
-    }
-    reverse(x,x+n1);reverse(y,y+n2);
-    for(int i=n2;i<n1;i++){
-    	y[i]=0;
-    }
-    int n=0, nho=0;//n: so chu so trong phep cong(n=n1 or n=n1+1; nho: bien nho)
-    int tmp;
-    for(int i=0;i<n1;i++){
-        tmp = x[i]+y[i]+nho;
-        z[n++]=tmp%10;
-        nho=tmp/10;
-    }
-    if(nho==1){
-    	z[n++]=nho;
-    }
-    for(int i=n-1;i>=0;i--){
-    	cout<<z[i];
-    }
+    if (carry > 0) res.push_back(carry + '0');
+    reverse(res.begin(), res.end());
+    return res;
 }
-int main(){
-	int t;
-	cin>>t;
-	cin.ignore();
-	while(t--){
-		string a,b;
-		cin>>a>>b;
-		if(a.size()>b.size()){
-			cong(a,b);
-		} else {
-			cong(b,a);
-		}
-		cout<<endl;
-	}
+
+int main() {
+    int t; cin >> t;
+    while (t--){
+        string X, Y;
+        cin >> X >> Y;
+        cout << addBigNumbers(X, Y) << "\n";
+    }
 }
