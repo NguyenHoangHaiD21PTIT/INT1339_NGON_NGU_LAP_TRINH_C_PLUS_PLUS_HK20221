28f17a35b3279c5c06da44c7d1004fec49f6a781
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-23 08:21:37 +0700

    Update CPP0307 - HIEU HAI TAP TU.cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 3. KIỂU DỮ LIỆU STRING VÀ ÁP DỤNG/CPP0307 - HIEU HAI TAP TU.cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 3. KIỂU DỮ LIỆU STRING VÀ ÁP DỤNG/CPP0307 - HIEU HAI TAP TU.cpp
@@ -5,26 +5,16 @@ int main(){
 	cin>>t;
 	cin.ignore();
 	while(t--){
-		string c1;
+		string c1, c2;
 		getline(cin, c1);
-		string c2;
 		getline(cin, c2);
-		stringstream ss1(c1);
-		stringstream ss2(c2);
-		string token1;
-		string token2;
-		set<string>s1;
-		set<string>s2;
-		while(ss1 >>token1){
-			s1.insert(token1);
-		}
-		while(ss2 >>token2){
-			s2.insert(token2);
-		}
+		stringstream ss1(c1), ss2(c2);
+		string token1, token2;
+		set<string>s1, s2;
+		while(ss1 >> token1) s1.insert(token1);
+		while(ss2 >> token2) s2.insert(token2);
 		for(string x:s1){
-			if(s2.find(x)==s2.end()){
-				cout<<x<<" ";
-			}
+			if(s2.find(x) == s2.end()) cout<<x<<" ";
 		}
 		cout<<endl;
 	}
