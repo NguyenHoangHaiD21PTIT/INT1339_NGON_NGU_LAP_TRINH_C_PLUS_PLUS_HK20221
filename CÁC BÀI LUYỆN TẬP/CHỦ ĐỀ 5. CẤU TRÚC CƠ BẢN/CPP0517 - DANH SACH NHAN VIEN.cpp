334c7d8266fbba8bf715ea5326cbf76f4b767713
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-23 08:04:57 +0700

    Update CPP0517 - DANH SACH NHAN VIEN.cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 5. CẤU TRÚC CƠ BẢN/CPP0517 - DANH SACH NHAN VIEN.cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 5. CẤU TRÚC CƠ BẢN/CPP0517 - DANH SACH NHAN VIEN.cpp
@@ -1,32 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 struct NhanVien{
-    string manv;//ma nhan vien
-    string ten;
-    string gioi;
-    string ns;//ngay sinh
-    string dc;//dia chi
-    string ms;//ma so thue
-    string ngayky;
+    string manv, ten, gioi, ns, dc, ms, ngayky;//ns: Ngày sinh, dc: Địa chỉ, ms: Mã số thuế
 };
 void nhap(NhanVien &a){
 	cin.ignore();
 	getline(cin, a.ten);
-	cin>>a.gioi>>a.ns;
+	cin >> a.gioi >> a.ns;
 	cin.ignore();
 	getline(cin, a.dc);
-	cin>>a.ms>>a.ngayky;
+	cin >> a.ms >> a.ngayky;
 }
 void inds(NhanVien ds[], int n){
-	for(int i=0;i<n;i++){
-		ds[i].manv=to_string(i+1);
-		int k= 5 - ds[i].manv.size();
-		while(k--){
-			cout<<"0";
-		}
-		cout<<ds[i].manv<<" ";
-		cout <<ds[i].ten<<" "<<ds[i].gioi<<" "<<ds[i].ns<<" "<<ds[i].dc<<" "<<ds[i].ms<<" "<<ds[i].ngayky;
-	    cout <<endl;
+	for(int i = 0; i < n; i++){
+		ds[i].manv = string(5 - to_string(i + 1).size(), '0') + to_string (i+1);
+		cout << ds[i].manv << " " << ds[i].ten << " " << ds[i].gioi << " " << ds[i].ns << " " << ds[i].dc << " " << ds[i].ms << " " << ds[i].ngayky << endl;
 	}
 }
 int main(){
