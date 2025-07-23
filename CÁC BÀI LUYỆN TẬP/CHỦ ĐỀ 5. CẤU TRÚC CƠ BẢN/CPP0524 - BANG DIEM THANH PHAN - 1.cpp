a875fdbe92001dca7e0bd00e78d7fb140b3a7d82
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-23 08:52:39 +0700

    Update CPP0524 - BANG DIEM THANH PHAN - 1.cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 5. CẤU TRÚC CƠ BẢN/CPP0524 - BANG DIEM THANH PHAN - 1.cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 5. CẤU TRÚC CƠ BẢN/CPP0524 - BANG DIEM THANH PHAN - 1.cpp
@@ -1,38 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-struct SinhVien{
-	string masv, hoten, lop;
-	double d1,d2,d3;//diem thi 3 mon
+
+struct SVien {
+    string msv, name, lop;
+    double diem1, diem2, diem3;
 };
-void nhap(SinhVien &a){
-	cin>>a.masv;
-	cin.ignore();
-	getline(cin, a.hoten);
-	cin>>a.lop;
-	cin>>a.d1>>a.d2>>a.d3;
+
+bool cmp(const SVien &a, const SVien &b) {
+    return a.msv < b.msv;
 }
-bool cmp(SinhVien a, SinhVien b){
-	if(a.masv!=b.masv){
-		return a.masv<b.masv;
-	}
-}
-void sap_xep(SinhVien ds[], int n){
-	sort(ds,ds+n,cmp);
-}
-void in_ds(SinhVien ds[], int n){
-	for(int i=0;i<n;i++){
-		cout <<i+1<<" "<<ds[i].masv<<" "<<ds[i].hoten<<" "<<ds[i].lop<<" ";
-		cout<<fixed<<setprecision(1)<<ds[i].d1<<" "<<ds[i].d2<<" "<<ds[i].d3<<endl;
-	}
-}
-int main(){
+
+int main() {
     int n;
     cin >> n;
-    struct SinhVien *ds = new SinhVien[n];
-    for(int i = 0; i < n; i++) {
-    	nhap(ds[i]);
-	}
-	sap_xep(ds, n);
-    in_ds(ds,n);
-    return 0;
+    SVien a[n];
+    for (int i = 0; i < n; i++) {
+        cin.ignore();
+        getline(cin, a[i].msv);
+        getline(cin, a[i].name);
+        getline(cin, a[i].lop);
+        cin >> a[i].diem1 >> a[i].diem2 >> a[i].diem3;
+    }
+    sort(a, a + n, cmp);
+    for (int i = 0; i < n; i++) 
+        cout << i + 1 << " " << a[i].msv << " " << a[i].name << " " << a[i].lop << " " << fixed << setprecision(1) << a[i].diem1 << " "
+        << fixed << setprecision(1) << a[i].diem2 << " "
+        << fixed << setprecision(1) << a[i].diem3 << "\n";
 }
