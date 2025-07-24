1b4cda2df884ccee240b63614453b5bcb4b722db
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-24 12:07:27 +0700

    Update CTest034. SO MAY MAN TRONG MA TRAN.cpp

--- CÁC BÀI KIỂM TRA/KIỂM TRA/THUC HANH 24.07/CTest034. SO MAY MAN TRONG MA TRAN.cpp
+++ CÁC BÀI KIỂM TRA/KIỂM TRA/THUC HANH 24.07/CTest034. SO MAY MAN TRONG MA TRAN.cpp
@@ -3,27 +3,29 @@ using namespace std;
 int main() {
     int hang, cot;
     cin >> hang >> cot;
-    int a[100][100]; 
+    int a[100][100];
     for (int i = 0; i < hang; i++) {
         for (int j = 0; j < cot; j++) cin >> a[i][j];
     }
-    int Min = 20000, Max = -1;
+    int Min = 1e9, Max = -1e9;
     for (int i = 0; i < hang; i++) {
         for (int j = 0; j < cot; j++) {
-            if (a[i][j] < Min) Min = a[i][j];
-            if (a[i][j] > Max) Max = a[i][j];
+            Min = min(Min, a[i][j]);
+            Max = max(Max, a[i][j]);
         }
     }
     int res = Max - Min;
-    int check = 0;
-    cout << res << endl;
+    bool found = false;
     for (int i = 0; i < hang; i++) {
         for (int j = 0; j < cot; j++) {
             if (a[i][j] == res) {
+                if (!found) {
+                    cout << res << endl;
+                    found = true;
+                }
                 cout << "Vi tri [" << i << "][" << j << "]" << endl;
-                check = 1;
             }
         }
     }
-    if(!check) cout << "NOT FOUND";
+    if (!found) cout << "NOT FOUND";
 }
