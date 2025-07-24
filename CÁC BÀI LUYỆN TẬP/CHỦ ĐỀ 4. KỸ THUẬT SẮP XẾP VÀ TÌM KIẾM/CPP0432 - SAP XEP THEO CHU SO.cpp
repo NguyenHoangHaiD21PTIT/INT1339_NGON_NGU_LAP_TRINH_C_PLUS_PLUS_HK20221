24a514d55d5e335554641bed52eace99abc3322c
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-24 10:54:15 +0700

    Update CPP0432 - SAP XEP THEO CHU SO.cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 4. KỸ THUẬT SẮP XẾP VÀ TÌM KIẾM/CPP0432 - SAP XEP THEO CHU SO.cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 4. KỸ THUẬT SẮP XẾP VÀ TÌM KIẾM/CPP0432 - SAP XEP THEO CHU SO.cpp
@@ -1,25 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 bool cmp(string a, string b){
-	if(a+b>b+a){
-		return true;
-	}
-	return false;
+	return a + b > b + a;
 }
 int main(){
 	int t;
-	cin>>t;
+	cin >> t;
 	while(t--){
-		int n;
-		cin>>n;
-		vector<string>v(n);
-		for(int i=0;i<n;i++){
-			cin>>v[i];
-		}
-		sort(v.begin(),v.end(),cmp);
-		for(int i=0;i<n;i++){
-			cout<<v[i];
-		}
+		int n; cin >> n;
+		vector <string> v(n);
+		for(int i = 0;i < n; i++) cin >> v[i];
+		sort(v.begin(), v.end(), cmp);
+		for(int i = 0;i < n; i++) cout<<v[i];
 		cout<<endl;
 	}
 }
