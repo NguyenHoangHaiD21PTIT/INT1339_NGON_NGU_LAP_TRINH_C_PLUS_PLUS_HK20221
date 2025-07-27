5b6e718b8852f1a62de45d4f517e5f2df29ee5a2
NguyenHoangHaiD21PTIT <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2025-07-27 16:40:08 +0700

    Update CPP0761 - UOC SO CHUNG LON NHAT CUA SO NGUYEN LON.cpp

--- CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 7. CÁC BÀI TOÁN NÂNG CAO/CHỦ ĐỀ 7.3. ỨNG DỤNG TOÁN HỌC/CPP0761 - UOC SO CHUNG LON NHAT CUA SO NGUYEN LON.cpp
+++ CÁC BÀI LUYỆN TẬP/CHỦ ĐỀ 7. CÁC BÀI TOÁN NÂNG CAO/CHỦ ĐỀ 7.3. ỨNG DỤNG TOÁN HỌC/CPP0761 - UOC SO CHUNG LON NHAT CUA SO NGUYEN LON.cpp
@@ -1,20 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	int t;
-	cin>>t;
-	cin.ignore();
-	while(t--){
-		long long a;
-		cin>>a;
-		string b;
-		cin>>b;
-		long long du=0;
-		//Tim b%a
-		for(int i=0;i<b.size();i++){
-			du=(du*10+b[i]-'0')%a;
-		}
-		long long kq=__gcd(a, du);
-		cout<<kq<<endl;
+	int t; cin >> t;
+	while (t --){
+		long long a, res = 0; string b;
+		cin >> a >> b;
+		for(char x: b) res = (res * 10 + x - '0') % a; //b%a;
+		long long ans = __gcd(a, res);
+		cout << ans << endl;
 	}
 }
