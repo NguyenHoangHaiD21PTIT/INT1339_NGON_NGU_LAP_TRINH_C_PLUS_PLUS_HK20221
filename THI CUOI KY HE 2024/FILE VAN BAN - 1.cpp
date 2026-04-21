3e42d082dc4995558b5221ca5f4109a131c86b4c
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2026-04-22 00:03:29 +0700

    .

--- THI CUOI KY HE 2024/FILE VAN BAN - 1.cpp
+++ THI CUOI KY HE 2024/FILE VAN BAN - 1.cpp
@@ -1,16 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main(){
-    ifstream cin ("PTIT.in");
-    ofstream cout ("PTIT.Out");
-    int t;
-    cin>>t;
+    ifstream in ("PTIT.in");
+    ofstream out ("PTIT.Out");
+    int t; cin >> t;
     while(t--){
-        string s;
-        cin>>s;
+        string s; cin >> s;
         char x = s.back();
-        if(x=='0'||x=='2'||x=='4'||x=='6'||x=='8') cout<<"Yes"<<endl;
-        else cout<<"No"<<endl;
+        if(x == '0' || x == '2' ||x == '4'|| x == '6'|| x == '8') out << "Yes" << endl;
+        else out << "No" << endl;
     }
 }
