82c142be3baa7f5d8998d8429992eaa124ebd0f7
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2026-04-22 00:37:36 +0700

    .

---
+++ THI CUOI KY HE 2024/KIỂM TRA SỐ NGUYÊN TỐ - 2.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+ll checkNT(ll n){
+    if(n <= 1) return 0;
+    for(int i = 2; i <= sqrt(n); i++){
+        if(n % i == 0) return 0;
+    }
+    return 1;
+}
+
+ll tongCS(ll n){
+    int res = 0;
+    while(n){
+        res += n % 10;
+        n /= 10;
+    }
+    return res;
+}
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        ll n, s; cin >> n >> s;
+        if(checkNT(n) && tongCS(n) == s) cout << "Yes" << endl;
+        else cout << "No" << endl;
+    }
+}
+
+
